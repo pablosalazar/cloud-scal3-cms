31f1b9fdfcf6eae0dfbe814f02143b7d8bf5c5f8
Juan Pablo Salazar <pablo.salazar.restrepo@gmail.com>
2025-10-06 21:14:01 -0500

    build: add serverExternalPackages to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  serverExternalPackages: ['@libsql/client', 'libsql'],
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
